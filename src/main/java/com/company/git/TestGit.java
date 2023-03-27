54fc0430a53970d4ec44e7811dd23a566d303e03
shunStar <1470144973@qq.com>
2023-03-27 17:57:29 +0800

    first commit

---
+++ src/main/java/com/company/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.company.git;
+
+/**
+ * @author ：shunStar
+ * @description：TODO
+ * @date ：2023/3/27 17:51
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
